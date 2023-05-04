a3f5179743e9b44dcd7662e157da6fb356315b8b
Yoon Koh <ykoh2@chicagobooth.edu>
2023-05-04 14:08:42 +0000

    done with users and venues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,7 +12,7 @@
 
 class Comment < ApplicationRecord
   def commenter
-    my_id = self.id
+    my_id = self.author_id
     
     matching_users = User.where({ :id => my_id })
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "users", :action => "home" })
-  get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
+  get("/", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:username", { :controller => "users", :action => "show" })
   get("/insert_user_record", { :controller => "venues", :action => "create" })
   get("/update_users/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
+  get("/venues/:an_id", { :controller => "venues", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
+  get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
   
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
  
